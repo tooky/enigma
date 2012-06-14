f92060ca038cfe10b056a190f977d4b900cca791
Steve Tooke <steve@heavi.es>
2012-06-14 15:47:12 +0100

    Basic machine.

--- lib/machine.rb
+++ lib/machine.rb
@@ -1,8 +1,15 @@
 class Machine
-  def initialize(rotor1, rotor2, rotor3)
+  attr_reader :rotor_mechanism, :reflector
+  def initialize(rotor_mechanism, reflector=nil)
+    @rotor_mechanism = rotor_mechanism
+    @reflector = reflector
   end
 
-  def call(message)
+  def call(char)
+    start_index = ('A'..'Z').to_a.index(char)
+    input_index = rotor_mechanism.input(start_index)
+    reflected_index = reflector.reflect(input_index)
+    output_index = rotor_mechanism.output(reflected_index)
   end
 
   def reset
--- spec/machine_spec.rb
+++ spec/machine_spec.rb
@@ -2,5 +2,36 @@ require_relative '../lib/machine'
 
 describe Machine do
   context 'encoding/decoding a character' do
+    let(:rotor_mechanism) { double(:rotors).as_null_object }
+    let(:reflector) { double(:reflector).as_null_object }
+    let(:machine) { Machine.new(rotor_mechanism, reflector) }
+
+    it 'returns the coded character' do
+      rotor_mechanism.stub(:input => 4)
+      reflector.stub(:reflect => 6)
+      rotor_mechanism.stub(:output => 8)
+
+      machine.call('I')
+    end
+
+    it 'inputs the index of the alphabet character to the rotor mechanism' do
+      ('A'..'Z').each_with_index do |c,index|
+        rotor_mechanism.should_receive(:input).with(index)
+        machine.call(c)
+      end
+    end
+
+    it 'reflects the index from the input to the rotors' do
+      rotor_mechanism.stub(:input => 4)
+      reflector.should_receive(:reflect).with(4)
+      machine.call('E')
+    end
+
+    it 'passes the reflected index back to the rotor mechanism' do
+      rotor_mechanism.stub(:input => 4)
+      reflector.stub(:reflect => 6)
+      rotor_mechanism.should_receive(:output).with(6)
+      machine.call('E')
+    end
   end
 end
